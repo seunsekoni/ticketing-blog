c6e6b15111779b79ea6b9749492bc3ca42879b68
Seun Sekoni <sekonioluwaseun@gmail.com>
2018-02-05 03:17:08 +0100

    Added the html design and codes for the comment section in show.blade.php

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -20,6 +20,44 @@
       </form>
       <div class="clearfix"></div>
     </div>
+    @foreach($comments as $comment)
+      <div class="well well bs-component">
+        <div class="content">
+          {!! $comment->content !!}
+        </div>
+      </div>
+    @endforeach
+  </div>
+  <div class="container col-md-8 col-md-offset-2">
+    <div class="well well bs-component">
+      @if (session('status'))
+        <div class="alert alert-success">
+          {{ session('status') }}
+        </div>
+      @endif
+      @foreach ($errors->all() as $error)
+        <p class="alert alert-danger">{{ $error }}</p>
+      @endforeach
+      <form class="form-horizontal" method = "post" action="/comment" >
+        <!-- <input type="hidden" name="_token" value="{!! csrf_token() !!}"> -->
+        {{ csrf_field() }}
+        <input type="hidden" name="post_id" value="{!! $ticket->id !!}">
+        <fieldset>
+          <legend>Reply</legend>
+            <div class="form-group">
+              <div class="col-lg-12">
+              <textarea class="form-control" rows="3" id="content" name="content"></textarea>
+              </div>
+            </div>
+            <div class="form-group">
+              <div class="col-lg-10 col-lg-offset-2">
+                <button class="btn btn-default" type="reset">Cancel</button>
+                <button type="submit" class="btn btn-primary">Post</button>
+              </div>
+            </div>
+        </fieldset>
+      </form>
+    </div>
   </div>
 @endsection
 
